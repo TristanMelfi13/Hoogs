e006a87340af4b88857564d80ae28915ee31b0f5
TristanMelfi13 <110581919+TristanMelfi13@users.noreply.github.com>
2023-11-17 14:23:39 -0500

    Update Hoog_Vs_Wats.py

--- Hoog_Vs_Wats.py
+++ Hoog_Vs_Wats.py
@@ -1,38 +1,47 @@
 import pandas as pd
-
-Seq_Length = 12
-Num_Strands = 2
 CurrentFrame = pd.read_csv("Structures/Duplex0.gro")
 
 
-class Nucleotide:
+class Residue:
     Name = ""
     Atom_Locations = {}
 
     def __init__(self, name):
         self.Name = name
 
+    def AddAtomLoc(self, AtomName, Xcor, Ycor, Zcor):
+        Location = [Xcor, Ycor, Zcor]
+        self.Atom_Locations[AtomName] = Location
 
-Nucleotide_List_For_CurrentFrame = []
+
 Name_List = []
 
 Atom_Data = CurrentFrame.values
 print(Atom_Data)
 
-Seen_End = 0
-Iter = 32
+def CreateObjects(Atom_Data, Seq_Length, Strands):
+    Iter = 1
+    Resi_List = []
+    Seen_End = 0
+    while Seen_End != Strands:
+        # Iter Accesses the ith atom information, [0] gets the data as a string,
+        # .split() removes whitespace, [0] accesses the full resi name, [2:3] Gets just the C/A/G/T
+        # Note that the first resi should be n - 2, & the last n + 1
+        Nucleo_Name = Atom_Data[Iter][0].split()[0]
+        Resi_List.append(Residue(Nucleo_Name))
+        for i in range(Iter, 10000):
+            Current_Atom = Atom_Data[i][0].split()
+            if Current_Atom[0] != Nucleo_Name:
+                Iter = i
+                break
+            else:
+                Resi_List[len(Resi_List) - 1].AddAtomLoc(Current_Atom[1], Current_Atom[3], Current_Atom[4], Current_Atom[5])
+        print(Atom_Data[370][0].split()[0][0:2])
+        break
+        if Nucleo_Name[0:2] == str(Strands):
+            Seen_End += 1
+    print(len(Resi_List[0].Atom_Locations))
+    return Resi_List
 
-while Seen_End != Num_Strands:
-    # Iter Accesses the ith atom information, [0] gets the data as a string,
-    # .split() removes whitespace, [0] accesses the full resi name, [2:3] Gets just the C/A/G/T
-    # Note that the first resi should be n - 2, & the last n + 1
-    Nucleo_Dict = {"C": 29, "A": 31, "G": 32, "T": 31}
-    Nucleo_Name = Atom_Data[Iter][0].split()[0]
-    Alter = 0
-    if "1D" == Nucleo_Name[0:2]:
-        Alter = -2
-    elif str(Seq_Length) == Nucleo_Name[0:2]:
-        Alter = 1
-    Atom_Count = Nucleo_Dict.get(Nucleo_Name[-1]) + Alter
-    break
 
+Current_Residue_Locations = CreateObjects(Atom_Data, Seq_Length=12,Strands=2)
